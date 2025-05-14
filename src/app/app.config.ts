2f4b90970c18310c116c4615a82a484204daef9f
Ali <ahmedalielm02@gmail.com>
2025-05-14 19:06:34 +0100

    2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-              provideRouter(routes),
+
+  providers: [ provideZoneChangeDetection({ eventCoalescing: true }),
+               provideRouter(routes),
              ]
 };
 
